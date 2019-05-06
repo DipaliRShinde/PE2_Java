e27f85ad813002d2807e3ffa54ca2b64a5cd4e6a
DipaliRShinde <drs1454006@gmail.com>
2019-05-06 11:40:22 +0530

    Factorial Program  added

--- src/main/java/com/stackroute/pe2/Factorial.java
+++ src/main/java/com/stackroute/pe2/Factorial.java
@@ -2,35 +2,50 @@ package com.stackroute.pe2;
 
 import java.util.Scanner;
 
-public class Factorial
-{
-    public static void main(String args[])
-    {
+public class Factorial {
+    public static void main(String args[]) {
         Factorial.factorialNumber();
+        Factorial.longFactorial();
     }
 
-    public static void factorialNumber()
-    {
-        Scanner s=new Scanner(System.in);
+    public static void factorialNumber() {
+        Scanner s = new Scanner(System.in);
         System.out.println("Enter Number");
-        int number=s.nextInt();
-        int fact=1;
+        int number = s.nextInt();
+        int fact = 1;
 
-        for(int i=1;i<number;i++)
-        {
+        for (int i = 1; i < number; i++) {
 
-            fact=1;
-            for(int j=1;j<=i;j++)
-            {
-                fact=fact*j;
+            fact = 1;
+            for (int j = 1; j <= i; j++) {
+                fact = fact * j;
             }
-            if(fact>Integer.MAX_VALUE)
-            {
-                System.out.println("The factorial of "+ i +" is out of range");
+            if (fact > 479001600) {
+                System.out.println("The factorial of " + i + " is out of range");
                 break;
             }
 
-            System.out.println("The factorial of "+ i + " is "+fact);
+            System.out.println("The factorial of " + i + " is " + fact);
+        }
+    }
+
+    public static void longFactorial() {
+        long i, fact = 1, j;
+        System.out.println("Long Factorials: ");
+
+        for (i = 1; i < Long.MAX_VALUE; i++) {                     //loop till maxvalue of long
+
+            for (j = 1; j <= i; j++) {
+                fact = fact * j;
+            }
+            if (i == 21) {
+                System.out.println("The factorial of " + i + " is out of range");   //when i reach 21 factorial should be out of range for long
+                break;
+            }
+            System.out.print("The factorial of " + i);
+            System.out.println(":" + fact);
+            fact = 1;
+
         }
     }
 }
